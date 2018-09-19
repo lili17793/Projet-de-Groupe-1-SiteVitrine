fda2bab39cbbcd76f92f91eac504f26db32f5ebd
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-09-19 10:01:43 +0200

    netoyage branche gallery

--- gallery.js
+++ gallery.js
@@ -1,15 +1,8 @@
-document.getElementById('open-close-button').addEventListener("click", changeIcon);
 
-function changeIcon(){
-	if (document.getElementById('button-content').className == "fa fa-chevron-down"){
-		document.getElementById('button-content').className = "fa fa-chevron-up";
-	} else if (document.getElementById('button-content').className == "fa fa-chevron-up") {
-		document.getElementById('button-content').className = "fa fa-chevron-down";
-	}
+function toTop() {
+	window.scroll({
+  		top: 0, 
+  		left: 0, 
+  		behavior: 'smooth' 
+	});		
 }
-
-$("#open-close-button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#open-close-button").offset().top
-    }, 1500);
-});
